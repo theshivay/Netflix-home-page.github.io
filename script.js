5d44dbaf330a7ec376eb2575320e035ea8496e40
Shivam Mishra <130253265+theshivay@users.noreply.github.com>
2023-11-21 23:28:52 +0530

    Add main files

---
+++ script.js
@@ -0,0 +1,66 @@
+function openDes1() {
+    var openDesc = document.getElementById('description1');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+function openDes2() {
+    var openDesc = document.getElementById('description2');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+function openDes3() {
+    var openDesc = document.getElementById('description3');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+function openDes4() {
+    var openDesc = document.getElementById('description4');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+function openDes5() {
+    var openDesc = document.getElementById('description5');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+function openDes6() {
+    var openDesc = document.getElementById('description6');
+    if (openDesc.style.display == 'none') {
+        openDesc.style.display = 'flex';
+    }
+    else {
+        openDesc.style.display = 'none';
+    }
+}
+// function openDes() {
+//     var openDesc = document.getElementsByClassName('faqbox1');
+//     if (openDesc.style.display == 'none') {
+//         openDesc.style.display = 'flex';
+//     }
+//     else if(openDesc.style.display=='flex') {
+//         openDesc.style.display = 'none';
+//     }
+//     else{
+//         console.log("Hello");
+//     }
+// }
